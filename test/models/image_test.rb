c67803e0c6e5f620967dfcb03110503ae3566f9a
Binwei Xu <binwei.xu@appfolio.com>
2019-08-15 14:25:32 -0700

    bx/yc - update image model to include tags using TagsAsTaggableOn Gem

--- test/models/image_test.rb
+++ test/models/image_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class ImageTest < ActiveSupport::TestCase
   test 'save succeeds' do
     image = Image.new(url: 'http://example.com/123.jpg')
+
     assert_predicate image, :valid?
   end
 
@@ -15,7 +16,21 @@ class ImageTest < ActiveSupport::TestCase
 
   test 'validates wrong url' do
     image = Image.new(url: 'foo')
+
     refute_predicate image, :valid?
     assert_equal ['must be a valid URL'], image.errors[:url]
+  end
+
+  def test_add_tags
+    image = Image.new(url: 'http://example.com', tag_list: 'example, demo')
+
+    assert_equal %w[example demo], image.tag_list
+  end
+
+  def test_remove_tags
+    image = Image.new(url: 'http://example.com', tag_list: 'example, demo')
+    image.tag_list.remove('demo')
+
+    assert_equal ['example'], image.tag_list
   end
 end
