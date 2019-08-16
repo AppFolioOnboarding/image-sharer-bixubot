94361014a32903d61ba343fa659d2d0ddbb91540
Binwei Xu <binwei.xu@appfolio.com>
2019-08-16 10:34:32 -0700

    bx/yc - add tag to images when displayed

--- test/controllers/images_controller_test.rb
+++ test/controllers/images_controller_test.rb
@@ -45,7 +45,7 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
     image2 = Image.create!(url: 'http://example.com/2')
     image3 = Image.create!(url: 'http://example.com/3')
 
-    get root_url
+    get images_path
     assert_select 'img' do |images|
       assert_equal images[0].attr('src'), image3.url
       assert_equal images[1].attr('src'), image2.url
@@ -61,8 +61,32 @@ class ImagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   def test_create__add_tag_to_db
-    post images_path, params: { image: { url: 'http://example.com', tag_list: 'example' } }
+    assert_difference 'Image.count' do
+      post images_path, params: { image: { url: 'http://example.com', tag_list: 'example' } }
+    end
 
     assert_equal ['example'], Image.last.tag_list
+  end
+
+  def test_show__tag_with_image
+    image = Image.create!(url: 'http://example.com', tag_list: 'example')
+
+    get image_path(image)
+
+    assert_response :ok
+    assert_select 'p', 'example'
+  end
+
+  def test_index__tags_with_images
+    Image.create!(url: 'http://example.com', tag_list: 'example1')
+    Image.create!(url: 'http://example.com', tag_list: 'example2')
+
+    get images_path
+
+    assert_response :ok
+    assert_select 'td' do |tags|
+      assert_includes tags[0].to_s, 'example2'
+      assert_includes tags[1].to_s, 'example1'
+    end
   end
 end
